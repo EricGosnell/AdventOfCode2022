15db40e9644946defe2f69eafd914f9d9461d42f
EricGosnell <eric.gosnell@colorado.edu>
2022-12-04 21:00:47 -0700

    Day 4

--- Day4/day4.cpp
+++ Day4/day4.cpp
@@ -1,5 +1,38 @@
 #include "../aoc2022.h"
+#include <sstream>
+#include <vector>
+
+vector<string> split (const string &s, char delim) {
+    vector<string> result;
+    stringstream ss(s);
+    string item;
+    while(getline(ss, item, delim)) {
+        result.push_back(item);
+    }
+    return result;
+}
 
 int main() {
+    ifstream input("day4in.txt");
+    string line;
+    int sum1 = 0;
+    int sum2 = 0;
+    while(getline(input,line)) {
+        vector<string> pair = split(line,',');
+        vector<string> left = split(pair[0],'-');
+        vector<string> right = split(pair[1],'-');
+        int l1 = stoi(left[0]);
+        int l2 = stoi(left[1]);
+        int r1 = stoi(right[0]);
+        int r2 = stoi(right[1]);
+        if (l1 >= r1 && l2 <= r2 || l1 <= r1 && l2 >= r2) {
+            sum1++;
+        }
+        if (l1 <= r1 && l2 >= r2 || l1 >= r1 && l1 <= r2 || l2 >= r1 && l2 <= r2) {
+            sum2++;
+        }
+    }
+    cout << "Part 1: " << sum1 << endl;
+    cout << "Part 2: " << sum2 << endl;
     return 0;
 }
